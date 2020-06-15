5480efccec99f2b07fec7c9898b9d236c0eb9338
christiann <chrisneundorf@googlemail.com>
2020-06-15 12:39:33 +0200

    Update

---
+++ hpecp-tutorialbox-container/build_and_run.sh
@@ -0,0 +1,7 @@
+# Build the Image
+docker build -t hpecp-tutorialbox
+# Remove the running container
+docker rm -f hpecp-tutorialbox
+# Create a new container
+docker run -d -p 8899:8888 --name hpecp-tutorialbox hpecp-tutorialbox:latest
+
